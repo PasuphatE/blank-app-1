97935ae2f8d809ab8467b81e4236e194b7a60e55
ieankungCU <ieankung@gmail.com>
2024-12-08 03:46:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
-st.title("🎈 Input Your OpenAI API Key 🎈")
+with st.sidebar:
+    st.title("🎈 Input Your OpenAI API Key 🎈")
 
+st.title("🎈 Input Some Text 🎈")
 st.write(
     "This application will help you to create your own world cloud"
 )
-with st.sidebar:
-    st.title("🎈 Input Some Text 🎈")
-    messages = st.container(height=300)
-    if prompt := st.chat_input("Type some text here"):
-        messages.chat_message("user").write(prompt)
-        messages.chat_message("assistant").write(f"Echo: {prompt}")
+    
+messages = st.container(height=300)
+if prompt := st.chat_input("Type some text here"):
+    messages.chat_message("user").write(prompt)
+    messages.chat_message("assistant").write(f"Echo: {prompt}")
