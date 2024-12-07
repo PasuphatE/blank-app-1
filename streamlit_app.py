b8022c21c24ef5a24f534ced1fe4b623ee59ca72
ieankungCU <ieankung@gmail.com>
2024-12-08 03:58:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,13 @@ st.write(
 #    messages.chat_message("assistant").write(f"Echo: {prompt}")
 
 
+txt = st.text_area(
+    "Text to analyze",
+    "It was the best of times, it was the worst of times, it was the age of "
+    "wisdom, it was the age of foolishness, it was the epoch of belief, it "
+    "was the epoch of incredulity, it was the season of Light, it was the "
+    "season of Darkness, it was the spring of hope, it was the winter of "
+    "despair, (...)",
+)
+
+st.write(f"You wrote {len(txt)} characters.")
